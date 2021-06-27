89a4b3b87d63dd9f806c3c53ece92dbdb936a8c9
dcnis <bgk.dennis@yahoo.de>
2021-06-27 11:22:51 +0200

    added Java 11 MapReduce project

---
+++ src/main/java/de/schmidtdennis/MapReduce/Main.java
@@ -0,0 +1,8 @@
+package de.schmidtdennis.MapReduce;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module de.schmidtdennis.MapReduce {
+
+}
